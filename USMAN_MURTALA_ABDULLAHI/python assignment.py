7277299032de1406dbac1cbd1faac61aaed48edb
USMAN ABDULLAHI <usydre82@gmail.com>
2022-01-28 12:56:02 +0100

    initial commit

---
+++ USMAN_MURTALA_ABDULLAHI/python assignment.py
@@ -0,0 +1,16 @@
+
+from cv2 import cv2
+from cv2 import INTER_AREA
+img = cv2.imread('jaf/mm.jpeg')
+scale_percent=0.50
+width=int(img.shape[1]*scale_percent)
+height=int(img.shape[0]*scale_percent)
+dimension=(width,height)
+
+resized=cv2.resize(img,dimension,interpolation=INTER_AREA)
+print(resized.shape)
+cv2.imshow('outout', resized)
+cv2.imwrite('resized_mm.jpeg' , resized)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
